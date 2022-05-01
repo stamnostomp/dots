438ab908762165f29f9b4bdac711415d990ddba3
сперма <43081223+stamnostomp@users.noreply.github.com>
2022-04-30 20:57:30 -0600

    colour update

---
+++ config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# kill already running bar 
+
+killall -q polybar
+
+# Launch polybar uning default config location ~/.config/polybar/config.ini
+
+polybar primary 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched ..."
---
+++ config/polybar/scripts/windowName.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WM_DESKTOP=$(xdotool getwindowfocus)
+
+if [ $WM_DESKTOP == "1321" ]; then
+
+	echo "Desktop"
+
+elif [ $WM_DESKTOP != "1883" ]; then
+
+	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
+	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
+	#kitty
+	if [ $WM_CLASS == 'kitty' ]; then
+		echo "kitty"
+	#everything else
+	else
+		echo "$WM_CLASS"
+	fi
+
+fi
