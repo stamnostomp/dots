c665d9a19335a965ac8fb6b02b53f90cd5ef7321
stamno <stamno@pm.me>
2022-05-14 16:44:55 -0600

    added desktop variation of dots

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,6 +6,7 @@ killall -q polybar
 
 # Launch polybar uning default config location ~/.config/polybar/config.ini
 
-polybar primary 2>&1 | tee -a /tmp/polybar.log & disown
+polybar primary 2>&1 | tee -a /tmp/polybar.log & 
+polybar secondary &
 
 echo "Polybar launched ..."
--- config/polybar/scripts/windowName.sh
+++ config/polybar/scripts/windowName.sh
@@ -2,19 +2,19 @@
 
 WM_DESKTOP=$(xdotool getwindowfocus)
 
-if [ $WM_DESKTOP == "1003" ]; then
+if [ $WM_DESKTOP == "496" ]; then
 
 	echo "Desktop"
 
 elif [ $WM_DESKTOP != "1883" ]; then
 
 	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
-	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
+	#WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
 	#kitty
-	if [ $WM_CLASS == 'kitty' ]; then
-		echo "kitty"
+	#if [ $WM_CLASS == 'kitty' ]; then
+		#echo "kitty"
 	#everything else
-	elif [ $WM_CLASS == 'Brave-browser' ]; then
+	if [ $WM_CLASS == 'Brave-browser' ]; then
 		echo "brave"
 	else
 		echo "$WM_CLASS"
