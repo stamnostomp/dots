511c31f87e1a631d99bcb4f700b5a7b28d41936c
stamno <stamno@pm.me>
2022-06-21 18:15:03 -0600

    small changes to the bar

--- .config/polybar/scripts/windowName.sh
+++ .config/polybar/scripts/windowName.sh
@@ -11,10 +11,10 @@ elif [ $WM_DESKTOP != "1883" ]; then
 	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
 	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
 	#kitty
-	#if [ $WM_CLASS == 'kitty' ]; then
-		#echo "kitty"
+	if [ $WM_CLASS == 'kitty' ]; then
+		echo "kitty"
 	#everything else
-	if [ $WM_CLASS == 'Brave-browser' ]; then
+  elif [ $WM_CLASS == 'Brave-browser' ]; then
 		echo "brave"
 	else
 		echo "$WM_CLASS"
