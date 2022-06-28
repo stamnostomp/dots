d444a6422b633801da31009ca6fe4fb8ed85c57b
stamno <stamno@pm.me>
2022-06-28 04:00:41 -0600

    changed fish to vi keys on start

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,4 @@ end
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/stamno/.ghcup/bin # ghcup-env
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/stamno/.ghcup/bin $PATH # ghcup-env
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/stamno/.ghcup/bin $PATH # ghcup-env
+fish_vi_keybindings
