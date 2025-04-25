7a259d4fa985710c5439db8e2ca3d80fc85720d8
stamno <your.email@example.com>
2025-04-24 23:00:24 -0600

    added ssh key concurency

--- nixos-config/modules/home/shell/fish.nix
+++ nixos-config/modules/home/shell/fish.nix
@@ -45,6 +45,12 @@
           echo "Entered Nix development environment"
         end
       end
+
+      # SSH agent setup
+        if test -z "$SSH_AUTH_SOCK"
+          eval (ssh-agent -c) > /dev/null
+          ssh-add ~/.ssh/id_ed25519 2>/dev/null
+        end
     '';
 
     shellAliases = {
