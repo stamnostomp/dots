6cb5576e92853499f1b7cc2c87b9af13cb828854
stamnostomp <stamno@pm.me>
2025-04-28 16:03:34 -0600

    added conig for t470s

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -116,9 +116,7 @@
             (nixpkgs.lib.mkIf (hostname == "laptop") {
               imports = [
                 # ThinkPad T470 has a Kaby Lake CPU
-                nixos-hardware.nixosModules.common-cpu-intel-kaby-lake
-                nixos-hardware.nixosModules.common-pc-laptop
-                nixos-hardware.nixosModules.common-pc-laptop-ssd
+                nixos-hardware.nixosModules.lenovo-thinkpad-t470s
               ];
             })
           ];
